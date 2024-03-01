1d5ebd82744f2722a2bc43b9034c46b01c6d4cf5
valeriogonini <valeriogonini@gmail.com>
2024-03-01 13:59:27 +0100

    welcome js

---
+++ js/app.js
@@ -0,0 +1,24 @@
+
+//esercizio 1
+
+let number = 100 //number
+
+console.log(number)
+
+//const PI = 3.14 //const
+//PI = 3.1416 // mi da errore
+//console.log(PI)
+
+//esercizio 2
+
+let radius = 8 //number
+let circle = (radius * 2 * 3.14); //number
+console.log(circle)
+ 
+//esercizio 3
+
+let name = "Carlo" //string
+name = "Marco" //string
+
+console.log(name)
+
