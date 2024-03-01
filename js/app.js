6ba920ce4e79c6c75afba2c7eaf9e2feadebc8e7
valeriogonini <valeriogonini@gmail.com>
2024-03-01 14:01:33 +0100

    add esercizio 2

--- js/app.js
+++ js/app.js
@@ -5,11 +5,13 @@ let number = 100 //number
 
 console.log(number)
 
-//const PI = 3.14 //const
+// esercizio 2
+
+const PI = 3.14 //const
 //PI = 3.1416 // mi da errore
 //console.log(PI)
 
-//esercizio 2
+//esercizio 2c
 
 let radius = 8 //number
 let circle = (radius * 2 * 3.14); //number
